2ba184cfcb8dcecb742b252faacb4e6aa54bf7b5
Joar Wathne <wathne@users.noreply.github.com>
2023-02-22 01:25:00 +0100

    WIP(ass-5): add files and specifications for assignment 5
    new file:   assignment-5/README.md
    new file:   assignment-5/samples/memory1_start.png
    new file:   assignment-5/samples/memory2_game.png
    new file:   assignment-5/samples/memory3_flipped.png
    new file:   assignment-5/samples/memory4_removed.png
    new file:   assignment-5/samples/memory5_winner.png
    new file:   assignment-5/start-vue/images/A.png
    new file:   assignment-5/start-vue/images/B.png
    new file:   assignment-5/start-vue/images/C.png
    new file:   assignment-5/start-vue/images/D.png
    new file:   assignment-5/start-vue/images/E.png
    new file:   assignment-5/start-vue/images/F.png
    new file:   assignment-5/start-vue/images/G.png
    new file:   assignment-5/start-vue/images/H.png
    new file:   assignment-5/start-vue/index.html
    new file:   assignment-5/start-vue/src/board.js
    new file:   assignment-5/start-vue/src/gamestat.js
    new file:   assignment-5/start-vue/style.css
    new file:   assignment-5/start/images/A.png
    new file:   assignment-5/start/images/B.png
    new file:   assignment-5/start/images/C.png
    new file:   assignment-5/start/images/D.png
    new file:   assignment-5/start/images/E.png
    new file:   assignment-5/start/images/F.png
    new file:   assignment-5/start/images/G.png
    new file:   assignment-5/start/images/H.png
    new file:   assignment-5/start/index.html
    new file:   assignment-5/start/style.css

---
+++ assignment-5/start-vue/src/gamestat.js
@@ -0,0 +1,25 @@
+const gameStatC = {
+    //need to make this component dynamic.
+    template: `
+    <div id="gamestat">
+        <div id="player1">
+            <p>
+                <span>Player 1:</span>&nbsp;<span class="score"> 0</span>
+            </p>
+            <p>
+                <span>0&nbsp;flips</span>
+            </p>
+        </div>
+        <div id="stat">
+            <span id="playerWrapper">Player 1</span>
+        </div>
+        <div id="player2">
+            <p>
+                <span>Player 2:</span>&nbsp;<span class="score">0</span>
+            </p>
+            <p>
+                <span>0&nbsp;flips</span>
+            </p>
+        </div>
+    </div>`
+}
