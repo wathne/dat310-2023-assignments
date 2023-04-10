6398f157efdd807dbbb4b661614ed1d607476041
Joar Wathne <wathne@users.noreply.github.com>
2023-04-10 19:45:20 +0200

    WIP(ass-8): add login functionality
    modified:   app.py
    new file:   templates/base.html
    new file:   templates/form_login.html
    new file:   templates/form_login_error.html
    new file:   templates/form_login_success.html
    new file:   templates/session_info.html
    new file:   templates/user_info.html

--- assignment-8/app.py
+++ assignment-8/app.py
@@ -1,15 +1,35 @@
 """Flask app.
 """
 
-from flask import Flask
-from flask import g
+#from flask import current_app # current_app is a LocalProxy.
+from flask import Flask # current_app real type.
+from flask import g # g is a LocalProxy.
 from flask import redirect
+from flask import render_template
+from flask import request # request is a LocalProxy.
+from flask import session # session is a LocalProxy.
 from flask import url_for
+from flask.ctx import _AppCtxGlobals as ACG # g real type.
+from flask.sessions import SecureCookieSession as SCS # session real type.
+from flask.wrappers import Request # request real type.
+from flask.wrappers import Response
+#from setup_db import create_user_table
+#from setup_db import create_address_table
+#from setup_db import add_user
+#from setup_db import insert_address
+#from setup_db import update_address
+#from setup_db import delete_address
+#from setup_db import get_user_addresses
+from setup_db import get_user_by_name
+#from setup_db import get_user_by_id
+from setup_db import get_hash_for_login
 from sqlite3 import connect
 from sqlite3 import Connection
 from sqlite3 import Error as AnySqlite3Error
 from typing import cast
-from werkzeug.wrappers.response import Response
+from werkzeug.local import LocalProxy as LP
+from werkzeug.security import check_password_hash
+from werkzeug.wrappers.response import Response as WerkzeugResponse
 
 
 _database_path: str = r"./database.db"
@@ -17,39 +37,189 @@ app: Flask = Flask(import_name=__name__)
 app.secret_key = "7503da98974f91e5945705068e8e9b07"
 
 
-def get_db() -> Connection | None:
-    if "db" not in g:
+def get_database_connection() -> Connection | None:
+    # pylint: disable=protected-access
+    acg: ACG = cast(LP[ACG], g)._get_current_object()
+    if "db_con" not in acg:
         try:
-            g.db = connect(_database_path)
+            acg.db_con = connect(database=_database_path)
         except AnySqlite3Error as err:
             print(err)
             return None
     try:
-        g.db.cursor()
+        acg.db_con.cursor()
     except AnySqlite3Error as err:
         print(err)
         return None
-    return cast(Connection, g.db)
+    return cast(Connection, acg.db_con)
 
 
 @app.teardown_appcontext
-# pylint: disable-next=unused-argument
-def teardown_db(exc: BaseException | None = None) -> None:
-    if "db" in g:
-        g.db.close()
+def teardown_database_connection(exc: BaseException | None = None) -> None:
+    # pylint: disable=protected-access
+    acg: ACG = cast(LP[ACG], g)._get_current_object()
+    if exc is not None:
+        print(exc)
+    if "db_con" in acg:
+        try:
+            acg.db_con.close()
+        except Exception:
+            pass
+
+
+def load_user() -> None:
+    # pylint: disable=protected-access
+    acg: ACG = cast(LP[ACG], g)._get_current_object()
+    acg.user = None
+
+    scs: SCS = cast(LP[SCS], session)._get_current_object()
+    scs_username: str | None = scs.get(key="username", default=None)
+    scs_password: str | None = scs.get(key="password", default=None)
+    if scs_username is None:
+        return None
+    if scs_password is None:
+        return None
+
+    db_con: Connection | None = get_database_connection()
+    if db_con is None:
+        return None
+    db_user: dict[str, str | int | None] | None = None
+    db_user_username: str | None = None
+    db_user_userid: int | None = None
+    db_user_pwhash: str | None = None
+
+    try:
+        db_user = get_user_by_name(conn=db_con, username=scs_username)
+    except AnySqlite3Error as err:
+        raise AnySqlite3Error(
+            f"Could not get user '{scs_username}' from database."
+        ) from err
+    if db_user is None:
+        return None
+
+    db_user_username = cast(str | None, db_user.get("username", None))
+    db_user_userid = cast(int | None, db_user.get("userid", None))
+    if db_user_username is None:
+        return None
+    if db_user_userid is None:
+        return None
+
+    try:
+        db_user_pwhash = get_hash_for_login(conn=db_con, username=scs_username)
+    except AnySqlite3Error as err:
+        raise AnySqlite3Error(
+            f"Could not get user '{scs_username}' password hash from database."
+        ) from err
+    if db_user_pwhash is None:
+        return None
+
+    if check_password_hash(pwhash=db_user_pwhash, password=scs_password):
+        acg.user = db_user_userid
+
+
+@app.before_request
+def before_request() -> WerkzeugResponse | Response | tuple[str, int] | None:
+    # pylint: disable=protected-access
+    acg: ACG = cast(LP[ACG], g)._get_current_object()
+    request_: Request = cast(LP[Request], request)._get_current_object()
+    db_con: Connection | None = get_database_connection()
+
+    print(f"request_.endpoint: {request_.endpoint}") # TODO: Delete this.
+    print(request_) # TODO: Delete this.
+
+    if request_.endpoint == "static":
+        #return None
+        pass # TODO: Skip load_user() if style.css.
+
+    # Do not call load_user() if endpoint is in whitelist.
+    whitelist: set[str] = {
+        "login",
+        "user_info",
+        "clear_user",
+        "session_info",
+        "clear_session",
+    }
+    if request_.endpoint in whitelist:
+        print(f"{request_.endpoint} is on whitelist") # TODO: Delete this.
+        return None
+
+    if db_con is None:
+        return ("No database connection.", 500)
+
+    try:
+        load_user()
+    except AnySqlite3Error as err:
+        print(err)
+        pass # TODO: Do something here?
+
+    if acg.user is None:
+        return redirect(
+            location=url_for(
+                endpoint="login",
+            ),
+            code=302,
+        )
+
+    return None
+
+
+@app.route("/user_info")
+def user_info() -> tuple[str, int]:
+    # pylint: disable=protected-access
+    scs: SCS = cast(LP[SCS], session)._get_current_object()
+    scs_username: str | None = scs.get(key="username", default=None)
+    scs_password: str | None = scs.get(key="password", default=None)
+    return (render_template(
+        template_name_or_list="user_info.html",
+        username=scs_username,
+        password=scs_password,
+    ), 200)
+
+
+@app.route("/clear_user")
+def clear_user() -> WerkzeugResponse | Response:
+    # pylint: disable=protected-access
+    scs: SCS = cast(LP[SCS], session)._get_current_object()
+    scs.pop(key="username", default=None)
+    scs.pop(key="password", default=None)
+    return redirect(
+        location=url_for(
+            endpoint="user_info",
+        ),
+        code=302,
+    )
+
+
+@app.route("/session_info")
+def session_info() -> tuple[str, int]:
+    # pylint: disable=protected-access
+    scs: SCS = cast(LP[SCS], session)._get_current_object()
+    return (render_template(
+        template_name_or_list="session_info.html",
+        scs=scs,
+    ), 200)
+
+
+@app.route("/clear_session")
+def clear_session() -> WerkzeugResponse | Response:
+    # pylint: disable=protected-access
+    scs: SCS = cast(LP[SCS], session)._get_current_object()
+    scs.clear()
+    return redirect(
+        location=url_for(
+            endpoint="session_info",
+        ),
+        code=302,
+    )
 
 
 @app.route("/")
 @app.route("/index")
-def index() -> Response | tuple[str, int]:
-    db: Connection | None = get_db()
-    if db is None:
-        print("db is None.") # TODO: Delete this.
-        print(f"db: {db}") # TODO: Delete this.
-        return ("No database connection.", 500)
+def index() -> WerkzeugResponse | Response:
+    # pylint: disable=protected-access
+    acg: ACG = cast(LP[ACG], g)._get_current_object()
+    print(f"acg.user: {acg.user}") # TODO: Delete this.
 
-    print("db is not None.") # TODO: Delete this.
-    print(f"db: {db}") # TODO: Delete this.
     return redirect(
         location=url_for(
             endpoint="static",
@@ -57,6 +227,49 @@ def index() -> Response | tuple[str, int]:
         ),
         code=302,
     )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login() -> tuple[str, int]:
+    # pylint: disable=protected-access
+    acg: ACG = cast(LP[ACG], g)._get_current_object()
+    request_: Request = cast(LP[Request], request)._get_current_object()
+    scs: SCS = cast(LP[SCS], session)._get_current_object()
+    db_con: Connection | None = get_database_connection()
+
+    if request_.method == "POST":
+        scs["username"] = request_.form.get(
+            key="username",
+            default=None,
+            type=str,
+        )
+        scs["password"] = request_.form.get(
+            key="password",
+            default=None,
+            type=str,
+        )
+
+        if db_con is None:
+            return ("No database connection.", 500)
+
+        try:
+            load_user()
+        except AnySqlite3Error as err:
+            print(err)
+            pass # TODO: Do something here?
+
+        if acg.user is None:
+            return (render_template(
+                template_name_or_list="form_login_error.html",
+            ), 200)
+
+        return (render_template(
+            template_name_or_list="form_login_success.html",
+        ), 200)
+
+    return (render_template(
+        template_name_or_list="form_login.html",
+    ), 200)
 
 
 if __name__ == "__main__":
